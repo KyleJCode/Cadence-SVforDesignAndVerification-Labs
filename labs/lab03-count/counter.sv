c76efdfd734b247afb1fc763d1af7fbe7c783a67
KyleJCode <kylestyle05@gmail.com>
2026-05-05 15:26:52 -0400

    lab03 complete

---
+++ labs/lab03-count/counter.sv
@@ -0,0 +1,25 @@
+module counter(
+    input logic [4:0] data,
+    input logic load,
+    input logic enable,
+    input logic clk, 
+    input logic rst_,
+    output logic [4:0] count
+);
+
+    timeunit 1ns;
+    timeprecision 100ps;
+
+    always_ff @(posedge clk or negedge rst_) begin
+        if(!rst_)
+            count <= '0;
+        else 
+        begin
+            if(load)
+                count <= data;
+            else if(enable)
+                count <= count + 1;
+        end
+    end
+
+endmodule

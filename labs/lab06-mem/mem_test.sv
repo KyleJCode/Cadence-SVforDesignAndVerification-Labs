f96edaf08aec30e8f9ceec7f2113a8d66afa11b4
KyleJCode <kylestyle05@gmail.com>
2026-05-12 17:09:39 -0400

    lab06-mem complete

--- labs/lab06-mem/mem_test.sv
+++ labs/lab06-mem/mem_test.sv
@@ -10,6 +10,8 @@
 // 
 ///////////////////////////////////////////////////////////////////////////
 
+// Template provided by Cadence, solution by Kyle Jeter
+
 module mem_test ( input logic clk, 
                   output logic read, 
                   output logic write, 
@@ -36,36 +38,40 @@ end
 initial begin: memtest
   int error_status;
 
+    error_status = 0;
     $display("Clear Memory Test");
 
     for (int i = 0; i< 32; i++)
+      write_mem(i, 0, debug);
        // Write zero data to every address location
-
+       
     for (int i = 0; i<32; i++)
       begin 
        // Read every address location
-
+         read_mem (i, rdata, debug);
        // check each memory location for data = 'h00
-
+         error_status += check_func(i, rdata, 8'h00);
       end
 
    // print results of test
-
+   printstatus(error_status);
+   error_status = 0;
     $display("Data = Address Test");
 
     for (int i = 0; i< 32; i++)
-       // Write data = address to every address location
+      write_mem (i, i, debug);
+      // Write data = address to every address location
        
     for (int i = 0; i<32; i++)
       begin
        // Read every address location
-
+         read_mem (i, rdata, debug);
        // check each memory location for data = address
-
+         error_status += check_func(i, rdata, i);
       end
 
    // print results of test
-
+    printstatus(error_status);
     $finish;
 
 end : memtest
@@ -83,7 +89,7 @@ task read_mem(input [4:0] rd_addr, output [7:0] rd_data, input debug = 0);
       $display("Debug Read: Addr Value = %0d, Data Value = %0h", rd_addr, rd_data);
 endtask : read_mem
 
-task write_mem(input [4:0] wr_addr, input [7:0] wr_data);
+task write_mem(input [4:0] wr_addr, input [7:0] wr_data, input debug = 0);
    @(negedge clk)
    addr <= wr_addr;
    data_in <= wr_data;
@@ -97,7 +103,18 @@ endtask : write_mem
 
 // add result print function
 function void printstatus(input int status);
-
+   if(status == 0)
+      $display("All tests passed.");
+   else
+      $display("%0d Errors Occurred.", status);
 endfunction : printstatus
+
+function int check_func(input [4:0] address, input [7:0] actual, expected);
+  if (actual !== expected) begin
+     $display("ERROR:  Address:%h  Data:%h  Expected:%h", address, actual, expected);
+     return 1;
+  end
+   return 0;
+endfunction: check_func
 
 endmodule
